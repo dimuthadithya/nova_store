908882c392ec54e3cc96858d5bf8568e22ad17d4
dimuthx353 <dimuthx353@gmail.com>
2025-09-21 12:46:03 +0530

    Remove unused SwiperSlide from Slider component to streamline code and improve performance

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -68,12 +68,6 @@ export default function Slider() {
         </SwiperSlide>
         <SwiperSlide>
           <img
-            src='https://images.unsplash.com/photo-1757386117955-c491ad384a26?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            alt=''
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
             src='https://www.nanotek.lk/storage/home-slider/xkIXZX64tsukEQYFC7Btc5WW04KWCjGXrRkDtKjX.webp'
             alt=''
           />
