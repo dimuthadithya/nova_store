c48efd2a225ebb08f4398f3bdd05d870dcf5ddae
dimuthx353 <dimuthx353@gmail.com>
2025-09-21 15:39:14 +0530

    Fix SVG attribute for consistent camelCase usage in SideMenu component

--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -121,7 +121,7 @@ function SideMenu() {
                   <path
                     stroke='currentColor'
                     strokeLinecap='round'
-                    stroke-linejoin='round'
+                    strokeLinejoin='round'
                     strokeWidth='2'
                     d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
                   />
